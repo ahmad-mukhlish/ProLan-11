f5dbc6d3a4d05f4eee78a921f87a690bec9a722c
ahmad-mukhlish <ahmad_mukhlish_s@yahoo.co.id>
2017-02-28 21:17:40 +0700

    Add files via upload

---
+++ Tugas 2 - Mahasiswa/mahasiswa.java
@@ -0,0 +1,9 @@
+public class mahasiswa {
+   public static void main(String agrs[]) {
+     System.out.println("Nama   : Ahmad Mukhlis Saputra") ;
+     System.out.println("NIM    : 10115134") ;
+     System.out.println("Alamat : Jl. Papandayan IV no. 32 Damar Mas Regency Banjaran") ;
+
+   }
+
+}
