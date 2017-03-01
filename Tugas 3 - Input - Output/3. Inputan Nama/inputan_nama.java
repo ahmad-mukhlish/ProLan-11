675e70f5d74414ca27ee2306503727c70e1953b1
ahmad-mukhlish <ahmad_mukhlish_s@yahoo.co.id>
2017-03-01 22:53:35 +0700

    Tambah Komentar dan Edit Output
    Penambahan beberapa komentar dan mencoba output dengan penggunaan
    operator +

--- Tugas 3 - Input - Output/3. Inputan Nama/inputan_nama.java
+++ Tugas 3 - Input - Output/3. Inputan Nama/inputan_nama.java
@@ -1,13 +1,20 @@
+//import Scanner 
 import java.util.Scanner ;
 
 public class inputan_nama {
-  public static void main(String args[]) {
-    Scanner input = new Scanner (System.in) ;
-    String nama, teman ;
+  public static void main(String args[]) 
+  {
+    //deklarasi Scanner
+	Scanner input = new Scanner (System.in) ;
+    
+	//deklarasi variabel
+	String nama, teman ;
 
-  System.out.print("Silakan Masukkan Nama Anda \t\t: ") ; nama = input.nextLine() ;
-  System.out.print("Silakan Masukkan Nama teman Anda \t: ") ; teman = input.nextLine() ;
+  	//inputan
+    System.out.print("Silakan Masukkan Nama Anda \t\t: ") ; nama = input.nextLine() ;
+    System.out.print("Silakan Masukkan Nama teman Anda \t: ") ; teman = input.nextLine() ;
 
-  System.out.printf("\nNama Anda adalah %s sedangkan nama teman Anda adalah %s \n",nama,teman) ;
+	//output
+    System.out.printf("\nNama Anda adalah %s sedangkan nama teman Anda adalah %s \n",nama,teman) ;
   }
 }
--- Tugas 3 - Input - Output/4. Inputan Mini Biodata/mini_biodata.java
+++ Tugas 3 - Input - Output/4. Inputan Mini Biodata/mini_biodata.java
@@ -1,20 +1,26 @@
+//import Scanner
 import java.util.Scanner ;
 public class mini_biodata {
   public static void main(String args[])
-  { Scanner masukkan = new Scanner (System.in) ;
-
+  { //deklarasi Scanner
+    Scanner masukkan = new Scanner (System.in) ;
+     
+	//deklarasi variabel 
     String  nama ;
     int tinggi ;
     char jk ;
     boolean status ;
-
+     
+	//inputan 
     System.out.print("Silakan Masukkan Nama Anda \t\t\t\t: ") ; nama = masukkan.nextLine() ;
     System.out.print("Silakan Masukkan Tinggi Anda \t\t\t\t: ") ; tinggi = masukkan.nextInt() ;
     System.out.print("Silakan Masukkan Jenis Kelamin Anda \t\t\t: ") ; jk = masukkan.next().charAt(0) ;
     System.out.print("Silakan Masukkan Status Menikah Anda (True/False) \t: ") ; status = masukkan.nextBoolean() ;
 
+	//hasil output
     System.out.printf("\nNama Anda adalah %s sedangkan tinggi Anda adalah %d \n",nama,tinggi) ;
-    System.out.printf("Jenis kelamin Anda adalah %c, dan status menikah Anda adalah %b\n",jk,status) ;
+    System.out.println("Jenis kelamin Anda adalah " + jk + " dan status menikah Anda adalah " + status) ;
+    
 
   }
 }
