d1448305d5f7936fcc97be3b6ab7f292a3244444
ahmad-mukhlish <ahmad_mukhlish_s@yahoo.co.id>
2017-03-08 15:37:06 +0700

    Tugas 5 TIpe Data

---
+++ Tugas 5 - Tipe Data/Tipe_data.java
@@ -0,0 +1,51 @@
+public class Tipe_data 
+{
+ public static void main(String args[])
+  {
+   //tipe data objek
+   String nama = "Mari belajar java" ;
+   System.out.println(nama.length()) ;
+      
+   //tipe data byte
+   byte maxByte = 127 ;
+   byte minByte = -128 ;
+   byte defaultByte = 0 ;
+   System.out.println(defaultByte) ;
+   
+   //tipe data short
+   short maxShort = 32767 ;
+   short minShort = -32767 ;
+   short defaultShort = 0 ;
+   System.out.println(defaultShort) ;
+   
+   //tipe data int
+   int maxInt = 2147483647 ;
+   int minInt = -2147483647 ;
+   int defaultInt = 0 ;
+   System.out.println(defaultInt) ; 
+ 
+   //tipe data long
+   long maxLong = 9223372036854775807L ;
+   long minLong = -9223372036854775808L  ;
+   long defaultLong = 0 ;
+   System.out.println(defaultLong) ;
+   
+   //tipe data float
+   float maxFloat = 3.4028234799999999999999999999999999999999f ;
+   float minFloat = -3.40282347999999999999999999999999999999999f  ;
+   float defaultFloat = 0.0f ;
+   System.out.println(defaultFloat) ;
+   
+   //tipe data double
+   double randomDouble = 3.4028234799999999999999999999999999999999 ;
+   double randomDouble1 = -3.40282347999999999999999999999999999999999  ;
+   double defaultDouble = 0.0 ;
+   System.out.println(randomDouble) ;
+   
+           
+      
+      
+      
+  }
+    
+}
