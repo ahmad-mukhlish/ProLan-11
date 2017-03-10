2775423221343b345d7a108c08b5ed59d9a7ed4c
ahmad-mukhlish <ahmad_mukhlish_s@yahoo.co.id>
2017-03-10 09:40:11 +0700

    Percobaan switch, this dan parameter

--- Tugas 4 - Kelas & objek/Makanan.java
+++ Tugas 4 - Kelas & objek/Makanan.java
@@ -1,7 +1,65 @@
-public class Makanan  
-{
+ 
+public class Makanan {
+
     //variabel global kelas makanan
-     String nama_makanan ;
-     long harga ;
+    String nama_makanan;
+    int harga;
+
+       
+    //method - method makanan
+    
+    
+    public void tampilkan()      
+    {Makanan pesanan = new Makanan() ;
+     pesanan.inisiasi_menu_makanan(1);
+     System.out.println("\n1. "+pesanan.nama_makanan+" "+pesanan.harga) ;   
+     pesanan.inisiasi_menu_makanan(2);
+     System.out.println("2. "+pesanan.nama_makanan+" "+pesanan.harga) ;
+     pesanan.inisiasi_menu_makanan(3);
+     System.out.println("3. "+pesanan.nama_makanan+" "+pesanan.harga) ;  
+     pesanan.inisiasi_menu_makanan(4);
+     System.out.println("4. "+pesanan.nama_makanan+" "+pesanan.harga) ;
+     pesanan.inisiasi_menu_makanan(5);
+     System.out.println("5. "+pesanan.nama_makanan+" "+pesanan.harga) ;  
      
+    }
+    
+    
+    public void inisiasi_menu_makanan(int menu) 
+    {
+ 
+        
+        switch (menu) 
+        {case 1 : {this.nama_makanan = "Ayam goreng + nasi" ; 
+                   this.harga = 18000 ;
+                   break ;}
+       
+         case 2 : {this.nama_makanan = "Sop iga + nasi" ;
+                   this.harga = 23000 ;
+                   break ;}
+         
+         case 3 : {this.nama_makanan = "Batagor spesial" ;
+                   this.harga = 15000 ;
+                   break ;}
+         
+         case 4 : {this.nama_makanan = "Cheese Burger" ;
+                   this.harga = 13000 ;
+                   break ;}
+         
+         case 5 : {this.nama_makanan = "Pasta mozarella" ;
+                   this.harga = 19000 ;
+                   break ;}
+         
+         default :{System.out.println("Menu yang Anda pilih tidak tersedia...") ;
+                   break ;}
+        }
+      
+
+        
+
+    }
+    
+    
+    
+    
 }
--- Tugas 4 - Kelas & objek/Pembeli.java
+++ Tugas 4 - Kelas & objek/Pembeli.java
@@ -1,114 +1,45 @@
  
+import java.util.Scanner ;
 
-import java.util.Scanner;
-
-/**
- *
- * @author GOODWARE1
- */
-class Pembeli {
+class Pembeli
+{
     //variabel global kelas pembeli
 
-    String nama;
-    int nomor_bangku;
-    Makanan pesanan;
-    long uang_masuk, kembalian;
+    String nama ;
+    int nomor_bangku ;
+    long uang_masuk, kembalian ;
 
     //inisiasi Scanner
-    Scanner input = new Scanner(System.in);
-    //inisiasi Pembeli objek global 
-    static Pembeli customer = new Pembeli();
-
-    public static Makanan ayam = new Makanan();
-    public static Makanan sop_iga = new Makanan();
-    public static Makanan batagor = new Makanan();
-    public static Makanan burger = new Makanan();
-    public static Makanan pasta = new Makanan();
-
-    //metoda pengendali kelas pembeli
-    public static void main(String args[]) {
+    Scanner input = new Scanner(System.in) ;
  
 
-        customer.data();
+    //method - method pembeli
+   
 
-        customer.liat_menu();
-
-        customer.transaksi();
-
-    }
-    
-    public void inisiasi_menu_makanan() {
-        ayam.nama_makanan = "Ayam goreng + nasi";
-        ayam.harga = 18000;
-
-        sop_iga.nama_makanan = "Sop iga + nasi";
-        sop_iga.harga = 23000;
-
-        batagor.nama_makanan = "Batagor spesial";
-        batagor.harga = 17000;
-
-        burger.nama_makanan = "Cheese Burger";
-        burger.harga = 13000;
-
-        pasta.nama_makanan = "Pasta mozarella";
-        pasta.harga = 24000;
-
-    }
-
-    public void tampil_menu_makanan() {
-        System.out.println();
-        System.out.println("1. " + ayam.nama_makanan + " Harganya " + ayam.harga);
-        System.out.println("2. " + sop_iga.nama_makanan + " Harganya " + sop_iga.harga);
-        System.out.println("3. " + batagor.nama_makanan + " Harganya " + batagor.harga);
-        System.out.println("4. " + burger.nama_makanan + " Harganya " + burger.harga);
-        System.out.println("5. " + pasta.nama_makanan + " Harganya " + pasta.harga);
-
-    }
-
-//method - method pembeli
-    private void liat_menu() {
-        Pembeli makan = new Pembeli();
-        makan.inisiasi_menu_makanan();
-        makan.tampil_menu_makanan();
-    }
-
-    private void data() {
-        System.out.print("Silakan Masukkan Nama Pembeli : ");
-        customer.nama = input.nextLine();
-        System.out.print("Silakan Masukkan Nomor Bangku Pembeli : ");
-        customer.nomor_bangku = input.nextInt();
+    public void data() 
+    {
+        System.out.print("\n\nSilakan Masukkan Nama Pembeli : ") ;
+        this.nama = input.nextLine() ;
+        System.out.print("Silakan Masukkan Nomor Bangku Pembeli : ") ;
+        this.nomor_bangku = input.nextInt() ;
 
     }
 
     public int pilih_menu() {
-        int menu;
-        System.out.print("\nSilakan Masukan Pilihan Pembeli : ");
-        menu = input.nextInt();
-        return menu;
+        int menu ;
+        System.out.print("\nSilakan Masukan Pilihan Pembeli : ") ;
+        menu = input.nextInt() ;
+        return menu ;
 
     }
+   
 
-    public void transaksi() {
-        int pilihan;
-        pilihan = customer.pilih_menu();
-        long harga;
-
-        if (pilihan == 1) {
-            harga = ayam.harga;
-        } else if (pilihan == 2) {
-            harga = sop_iga.harga;
-        } else if (pilihan == 3) {
-            harga = batagor.harga;
-        } else if (pilihan == 4) {
-            harga = burger.harga;
-        } else {
-            harga = pasta.harga;
-        }
-
-        System.out.print("Uang masuk pembeli : ");
-        customer.uang_masuk = input.nextLong();
-        customer.kembalian = customer.uang_masuk - harga;
-        System.out.println("Harga makanan " + harga + " Uang kembalian pembeli : " + customer.kembalian);
+    public void transaksi(int harga) 
+    { 
+        System.out.print("Uang masuk pembeli : ") ;
+        this.uang_masuk = input.nextLong() ;
+        this.kembalian = this.uang_masuk - harga ;
+        System.out.println("Harga makanan " + harga + " Uang kembalian pembeli : " + this.kembalian+"\n") ;
 
     }
 
